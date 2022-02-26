3cba88ad59d3792d5a1f0ccc94f95fd5b5967e7f
nghideptry <94179902+nghideptry@users.noreply.github.com>
2022-02-26 16:08:31 +0700

    Add

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -82,20 +82,41 @@ function myFunction() {
     }
 }
 
-// Scroll Top //
+// Pop Up //
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop()>300){
-        $('scrollButton').fadeIn(250);
-        }
-        else{
-        $('scrollButton').fadeOut(250);
-        }
-    });
-    $('scrollButton').click(function(){
-        $('html,body').animate(
-        {scrollTop:0},400
-        );
-    });
-});
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
